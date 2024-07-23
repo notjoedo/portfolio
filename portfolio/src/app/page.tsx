e69a3262b628b57d7ec60e11176abacaea1a0e7b
Joe <joedo2910@gmail.com>
2024-07-23 10:51:54 -0400

    animations

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,20 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 import Footer from "../components/footer";
 
 export default function Home() {
   return (
     <div className="mt-36 flex flex-col items-center justify-center">
-      <div className="relative overflow-hidden rounded-full border-2 border-[#A3B18A] shadow-sm shadow-[#DAD7CD]">
-        <Image
-          className="rounded-full"
-          src="/me.png"
-          width={400}
-          height={400}
-          alt="Joe"
-        />
+      <div
+        className="relative overflow-hidden rounded-full border-2 border-[#A3B18A] shadow-sm shadow-[#DAD7CD] transition ease-in delay-150 
+      hover:-translate-y-1 hover:scale-110 hover:border-[#DAD7CD] duration-150"
+      >
+        <Link href="/about">
+          <Image
+            className="rounded-full"
+            src="/me.png"
+            width={400}
+            height={400}
+            alt="Joe"
+          />
+        </Link>
       </div>
-      <div className="flex justify-between pt-10 text-4xl font-extralight tracking-wide text-[#DAD7CD]">
-        <h1>Hello, my name is Joe</h1>
+      <div className="w-max">
+        <h1
+          className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white
+         pr-5 mt-10 text-3xl font-light text-[#DAD7CD]"
+        >
+          Hello, my name is Joe.
+        </h1>
       </div>
       <Footer />
     </div>
