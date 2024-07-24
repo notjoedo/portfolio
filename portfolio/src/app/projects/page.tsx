4a391a62d7cc26549eb0d2c4b69d887e1d337fcb
Joe <joedo2910@gmail.com>
2024-07-24 12:42:35 -0400

    Update page.tsx

--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 export default function Projects() {
   return (
     <div className="flex flex-col justify-center items-center mt-20">
-      <div className="flex  space-x-40">
+      <div className="flex space-x-40 justify-center items-center">
         <Link
           href="https://cherienails.com"
           className="text-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:font-normal duration-150"
         >
           <Image
+            className="mt-20"
             src="/cheriewhite.png"
             alt="cherienails"
             width="500"
@@ -21,13 +22,7 @@ export default function Projects() {
             Nail Salon Website
           </h1>
           <p className="text-[#DAD7CD] font-light text-wrap max-w-96">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            A nail salon website created using Astro and Tailwind!
           </p>
         </div>
       </div>
