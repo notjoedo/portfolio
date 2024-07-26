81c3612eb76997500291ef0d7d6b56381dd8563a
Joe <joedo2910@gmail.com>
2024-07-26 15:11:08 -0400

    projects page responsiveness

--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 export default function Projects() {
   return (
-    <div className="flex flex-col justify-center items-center mt-20">
-      <div className="flex space-x-40 justify-center items-center">
+    <div className="flex flex-col justify-center items-center p-10 md:ml-10 xl:ml-0 md:mt-20">
+      <div className="flex flex-col md:flex-row md:space-x-20 lg:space-x-40 justify-center items-center">
         <Link
           href="https://cherienails.com"
           className="text-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:font-normal duration-150"
         >
           <Image
-            className="mt-20"
+            className="mt-10 md:mt-20 xl:mt-16 mb-10 md:mb-0"
             src="/cheriewhite.png"
             alt="cherienails"
             width="500"
             height="500"
           ></Image>
         </Link>
-        <div className="flex flex-col justify-center items-center space-y-10">
-          <h1 className="text-5xl text-[#DAD7CD] font-light tracking-wide pt-5">
+        <div className="flex flex-col justify-center items-center space-y-5">
+          <h1 className="text-2xl md:text-5xl text-[#DAD7CD] font-light tracking-wide pt-5">
             Nail Salon Website
           </h1>
           <p className="text-[#DAD7CD] font-light text-wrap max-w-96">
