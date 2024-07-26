376157fd8542e418b85ceef4a65cce1bf13fb072
Joe <joedo2910@gmail.com>
2024-07-26 14:55:58 -0400

    responsive about page

--- portfolio/src/app/about/page.tsx
+++ portfolio/src/app/about/page.tsx
@@ -4,8 +4,8 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 export default function About() {
   return (
     <div className="flex flex-col m-10 p-10 justify-center items-center">
-      <div className="flex flex-row md:flex-row items-center">
-        <div className="relative overflow-hidden rounded-full border-2 border-[#A3B18A] shadow-sm shadow-[#DAD7CD] mb-6 md:mb-0 md:mr-6 flex-shrink-0">
+      <div className="flex flex-col lg:flex-row md:space-y-10 lg:space-y-0 items-center">
+        <div className="justify-center items-center relative overflow-hidden rounded-full border-2 border-[#A3B18A] shadow-sm shadow-[#DAD7CD] mb-6 md:mb-0 flex-shrink-0">
           <Image
             className="rounded-full"
             src="/me.png"
@@ -16,29 +16,27 @@ export default function About() {
         </div>
         <div className="flex-row text-center md:text-left">
           <h1
-            className="animate-typeonce overflow-hidden whitespace-nowrap border-r-4 border-r-white
-         px-10 text-xl font-light text-[#DAD7CD] leading-10"
+            className="animate-typeonce overflow-hidden border-r-4 border-r-white px-4 sm:px-10 lg:text-xl text-sm sm:text-base font-light 
+          text-[#DAD7CD] leading-6 sm:leading-8 lg:leading-10 break-words whitespace-normal md:whitespace-nowrap"
           >
             I'm a student majoring in Computational Modeling & Data Analytics
-            and minoring in Computer Science at Virginia Tech! <br /> I love
-            full-stack development and learning all sorts of programming stuff.
-            Whether it's front-end design or back-end logic, <br /> I enjoy the
-            challenge and creativity that comes with building apps. In my free
-            time, I'm always exploring new tech and looking to level up my
-            skills. <br />
+            <br />
+            and minoring in Computer Science at Virginia Tech! <br />
+            I'm always exploring new tech and looking to level up my skills.{" "}
+            <br />
             Thanks for taking a moment to get to know me!
           </h1>
         </div>
       </div>
-      <div className="flex justify-start items-center bg-[#A3B18A] rounded-lg mt-20 drop-shadow-lg w-full">
-        <TabGroup className={"p-5 space-y-10"}>
-          <TabList className="space-x-10 font-bold text-3xl tracking-[-0.05em] mr-32 pr-52">
+      <div className="flex justify-start items-center bg-[#A3B18A] rounded-lg mt-20 drop-shadow-lg md:w-full">
+        <TabGroup className={"p-5 lg:space-y-10 "}>
+          <TabList className="flex flex-row space-x-10 font-bold text-2xl md:text-3xl tracking-[-0.05em] lg:mr-32 lg:pr-52">
             <Tab
               className={
                 "transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150"
               }
             >
-              TECH STACK
+              TECHSTACK
             </Tab>
             <Tab
               className={
@@ -49,13 +47,16 @@ export default function About() {
             </Tab>
           </TabList>
           <TabPanels>
-            <TabPanel>
-              <div className="flex flex-row px-5 pb-5 space-x-20">
+            <TabPanel className={"mt-5"}>
+              <div
+                className="flex flex-col md:flex-row justify-center md:justify-start md:flex-wrap items-center md:items-center 
+              space-y-20 md:space-y-5 xl:space-y-0 md:space-x-10 px-5 pb-5"
+              >
                 <Image
                   src={"/html-icon.svg"}
                   alt="html"
-                  width="88"
-                  height="88"
+                  width="100"
+                  height="100"
                 />
                 <Image src={"/css.svg"} alt="next" width="100" height="100" />
                 <Image
@@ -91,7 +92,10 @@ export default function About() {
               </div>
             </TabPanel>
             <TabPanel>
-              <div className="flex flex-row px-5 pb-5 space-x-20">
+              <div
+                className="flex flex-col md:flex-row justify-center md:justify-start md:flex-wrap items-center md:items-center 
+              space-y-20 md:space-y-5 xl:space-y-0 md:space-x-10 px-5 pb-5"
+              >
                 <Image
                   src={"/frontend.png"}
                   alt="react"
