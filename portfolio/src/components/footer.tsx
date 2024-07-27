f2b93a04bae38777ee7bd2327bdc0e09efc6150e
Joe <joedo2910@gmail.com>
2024-07-26 22:13:46 -0400

    Update footer.tsx

--- portfolio/src/components/footer.tsx
+++ portfolio/src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="absolute bottom-0 left-0 p-4 w-full flex justify-between items-end text-xs lg:text-xl text-[#DAD7CD] font-extralight">
       <div>
-        <h1>Last Updated: 7/22/2024</h1>
+        <h1>Last Updated: 7/26/2024</h1>
       </div>
       <div>
         <h1>joedo2910@gmail.com</h1>
