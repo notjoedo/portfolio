48e5a9f1fe282ed679e71611fc6ac7f9df95831b
Joe <joedo2910@gmail.com>
2024-07-30 01:06:24 -0400

    responsive update

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className="mt-12 md:mt-36 mb-24 flex flex-col items-center justify-center">
+    <div className="mt-0 md:mt-24 flex flex-col items-center justify-center">
       <div
         className="relative overflow-hidden rounded-full border-2 border-[#A3B18A] shadow-sm shadow-[#DAD7CD] transition ease-in delay-150 
       hover:-translate-y-1 hover:scale-110 hover:border-[#DAD7CD] duration-150 m-7 md:m-0"
@@ -12,8 +12,8 @@ export default async function Home() {
           <Image
             className="rounded-full"
             src="/me.png"
-            width={400}
-            height={400}
+            width={350}
+            height={350}
             alt="Joe"
             priority={true}
           />
@@ -22,7 +22,7 @@ export default async function Home() {
       <div className="w-max mb-10 md:mb-0">
         <h1
           className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white
-         pr-5 mt-10 text-3xl font-light text-[#DAD7CD] tracking-[-0.10em]"
+         pr-5 mt-0 md:mt-10 text-3xl font-light text-[#DAD7CD] tracking-[-0.10em]"
         >
           Hello, my name is Joe.
         </h1>
--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="p-5 tracking-[-0.10em] text-2xl sm:text-3xl text-[#DAD7CD] font-extralight flex flex-col sm:flex-row justify-between items-center sm:items-start">
+    <div className="p-5 tracking-[-0.10em] text-2xl sm:text-xl lg:text-3xl text-[#DAD7CD] font-extralight flex flex-col sm:flex-row justify-between items-center sm:items-start">
       <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:font-normal duration-150 mb-4 sm:mb-0">
         <Link href="/">Home</Link>
       </div>
